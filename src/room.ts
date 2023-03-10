8f3c7a72e2f4afb4cdd7cdd9e2775eee02c3d9f1
rolud <rolud.dev@gmail.com>
2023-03-10 10:55:46 +0100

    feat(room interface): added join and remove users or agent implementation

--- src/room.ts
+++ src/room.ts
@@ -38,7 +38,7 @@ export class Room implements IRoom {
     }
   }
 
-  public async createRoomWithUsers(users: string[], name?: string | undefined): Promise<Types.RoomDetails> {
+  public async createRoomWithUsers(users: string[], name?: string): Promise<Types.RoomDetails> {
     const currentFirebaseUser = auth().currentUser
     if (currentFirebaseUser === null) {
       throw new Error('[Halo@createRoomWithUsers] Firebase user not authenticated')
@@ -79,50 +79,201 @@ export class Room implements IRoom {
 
         return { ...(existingRoom.data() as Types.Room), users: usersDetails, agents: agentsDetails }
       }
-
-      const doc = firestore().collection(CollectionName.rooms).doc()
-
-      const room: Types.Room = {
-        id: doc.id,
-        createdBy: creatorData.id,
-        createdAt: firestore.Timestamp.now().toDate().toISOString(),
-        name: name ?? null,
-        usersIds: [creatorData.id, ...users],
-        removedUsersIds: [],
-        scope: users.length === 1 ? 'PRIVATE' : 'GROUP',
-        metadata: null,
-        lastMessage: null,
-        agentsIds: null,
-        tag: null,
-      }
-
-      await doc.set(room)
-
-      const usersDetails = (
-        await firestore().collection(CollectionName.users).where('id', 'in', room.usersIds).get()
-      ).docs.map((u) => userToUserDetails(u.data() as Types.User))
-
-      return {
-        ...room,
-        users: usersDetails,
-        agents: [],
-      }
     }
 
-    throw new Error('Method not implemented.')
+    const doc = firestore().collection(CollectionName.rooms).doc()
+
+    const room: Types.Room = {
+      id: doc.id,
+      createdBy: creatorData.id,
+      createdAt: firestore.Timestamp.now().toDate().toISOString(),
+      name: name ?? null,
+      usersIds: [creatorData.id, ...users],
+      removedUsersIds: [],
+      scope: users.length === 1 ? 'PRIVATE' : 'GROUP',
+      metadata: null,
+      lastMessage: null,
+      agentsIds: null,
+      tag: null,
+    }
+
+    await doc.set(room)
+
+    const usersDetails = (
+      await firestore().collection(CollectionName.users).where('id', 'in', room.usersIds).get()
+    ).docs.map((u) => userToUserDetails(u.data() as Types.User))
+
+    return {
+      ...room,
+      users: usersDetails,
+      agents: null,
+    }
   }
-  createRoomForAgents(tag: string): Promise<Types.RoomDetails> {
-    throw new Error('Method not implemented.')
+
+  public async createRoomForAgents(tag: string): Promise<Types.RoomDetails> {
+    const currentFirebaseUser = auth().currentUser
+    if (currentFirebaseUser === null) {
+      throw new Error('[Halo@createRoomForAgents] Firebase user not authenticated')
+    }
+
+    const creatorDoc = await firestore().collection(CollectionName.users).doc(currentFirebaseUser.uid).get()
+    if (!creatorDoc.exists) {
+      throw new Error('[Halo@createRoomForAgents] user not found')
+    }
+    const creatorData = creatorDoc.data() as Types.User
+
+    const doc = firestore().collection(CollectionName.rooms).doc()
+
+    const room: Types.Room = {
+      id: doc.id,
+      createdAt: firestore.Timestamp.now().toDate().toISOString(),
+      agentsIds: [],
+      createdBy: creatorData.id,
+      lastMessage: null,
+      metadata: null,
+      name: null,
+      removedUsersIds: [],
+      scope: 'AGENT',
+      tag,
+      usersIds: [creatorData.id],
+    }
+
+    await doc.set(room)
+
+    return {
+      ...room,
+      users: [creatorData],
+      agents: [],
+    }
   }
-  joinUser(userId: string, roomId: string): Promise<Types.RoomDetails> {
-    throw new Error('Method not implemented.')
+
+  public async joinUser(userId: string, roomId: string): Promise<Types.RoomDetails> {
+    const currentFirebaseUser = auth().currentUser
+    if (currentFirebaseUser === null) {
+      throw new Error('[Halo@joinUser] Firebase user not authenticated')
+    }
+
+    const user = await firestore().collection(CollectionName.users).doc(userId).get()
+    if (!user.exists) {
+      throw new Error('[Halo@joinUser] user not found')
+    }
+
+    const room = await firestore().collection(CollectionName.rooms).doc(roomId).get()
+    if (!room.exists) {
+      throw new Error('[Halo@joinUser] room not found')
+    }
+
+    const roomData = room.data() as Types.Room
+    if (roomData.scope !== 'GROUP') {
+      throw new Error('[Halo@joinUser] room scope must be GROUP')
+    }
+    if (roomData.usersIds.includes(userId)) {
+      throw new Error('[Halo@joinUser] user already in the room')
+    }
+
+    const usersIds = [...roomData.usersIds, userId]
+    const removedUsersIds = roomData.removedUsersIds.filter((uid) => uid !== userId)
+
+    await firestore().collection(CollectionName.rooms).doc(roomId).update({ usersIds, removedUsersIds })
+
+    const users = (await firestore().collection(CollectionName.users).where('id', 'in', usersIds).get()).docs.map((u) =>
+      userToUserDetails(u.data() as Types.User),
+    )
+
+    return {
+      ...roomData,
+      usersIds,
+      removedUsersIds,
+      users,
+      agents: null,
+    }
   }
-  joinAgent(agentId: string, roomId: string): Promise<Types.RoomDetails> {
-    throw new Error('Method not implemented.')
+
+  public async joinAgent(agentId: string, roomId: string): Promise<Types.RoomDetails> {
+    const currentFirebaseUser = auth().currentUser
+    if (currentFirebaseUser === null) {
+      throw new Error('[Halo@joinAgent] Firebase user not authenticated')
+    }
+
+    const agent = await firestore().collection(CollectionName.agents).doc(agentId).get()
+    if (!agent.exists) {
+      throw new Error('[Halo@joinAgent] agent not found')
+    }
+
+    const room = await firestore().collection(CollectionName.rooms).doc(roomId).get()
+    if (!room.exists) {
+      throw new Error('[Halo@joinAgent] room not found')
+    }
+
+    const roomData = room.data() as Types.Room
+    if (roomData.scope !== 'AGENT') {
+      throw new Error('[Halo@joinUser] room scope must be AGENT')
+    }
+    if (roomData.agentsIds?.includes(agentId)) {
+      throw new Error('[Halo@joinUser] agent already in the room')
+    }
+
+    const agentsIds = [...(roomData.agentsIds ?? []), agentId]
+    await firestore().collection(CollectionName.rooms).doc(roomId).update({ agentsIds })
+
+    const users = (
+      await firestore().collection(CollectionName.users).where('id', 'in', roomData.usersIds).get()
+    ).docs.map((u) => userToUserDetails(u.data() as Types.User))
+
+    const agents = (await firestore().collection(CollectionName.agents).where('id', 'in', agentsIds).get()).docs.map(
+      (u) => agentToAgentDetails(u.data() as Types.Agent),
+    )
+
+    return {
+      ...roomData,
+      agentsIds,
+      users,
+      agents,
+    }
   }
-  removeUser(userId: string, roomId: string): Promise<Types.RoomDetails> {
-    throw new Error('Method not implemented.')
+
+  public async removeUser(userId: string, roomId: string): Promise<Types.RoomDetails> {
+    const currentFirebaseUser = auth().currentUser
+    if (currentFirebaseUser === null) {
+      throw new Error('[Halo@removeUser] Firebase user not authenticated')
+    }
+
+    const user = await firestore().collection(CollectionName.users).doc(userId).get()
+    if (!user.exists) {
+      throw new Error('[Halo@removeUser] user not found')
+    }
+
+    const room = await firestore().collection(CollectionName.rooms).doc(roomId).get()
+    if (!room.exists) {
+      throw new Error('[Halo@removeUser] room not found')
+    }
+
+    const roomData = room.data() as Types.Room
+    if (roomData.scope !== 'GROUP') {
+      throw new Error('[Halo@removeUser] room scope must be GROUP')
+    }
+    if (!roomData.usersIds.includes(userId) || roomData.removedUsersIds.includes(userId)) {
+      throw new Error('[Halo@removeUser] user not in the room')
+    }
+
+    const removedUsersIds = [...roomData.removedUsersIds, userId]
+    const usersIds = roomData.usersIds.filter((uid) => uid !== userId)
+
+    await firestore().collection(CollectionName.rooms).doc(roomId).update({ usersIds, removedUsersIds })
+
+    const users = (await firestore().collection(CollectionName.users).where('id', 'in', usersIds).get()).docs.map((u) =>
+      userToUserDetails(u.data() as Types.User),
+    )
+
+    return {
+      ...roomData,
+      usersIds,
+      removedUsersIds,
+      users,
+      agents: null,
+    }
   }
+
   sendTextMessage(data: CreateTextMessagePayload): Promise<Types.MessageType.Any> {
     throw new Error('Method not implemented.')
   }
